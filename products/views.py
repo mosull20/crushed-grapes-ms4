f2811e1df133695800e990f337f85581f14adc44
mosull20 <mosullivan@mac.com>
2022-01-15 12:19:02 +0000

    Create product detail page

--- products/views.py
+++ products/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
     """ View to show products, sort products and handle search queries """
+    
     products = Product.objects.all()
 
     context = {
-        'products': products
+        'products': products,
     }
-
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ View to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
