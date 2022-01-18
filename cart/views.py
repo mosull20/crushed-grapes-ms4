59de3585b960e2785b60e3b6d59809ab58b9f0ef
mosull20 <mosullivan@mac.com>
2022-01-18 17:24:52 +0000

    Add further toast message content

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -15,16 +15,17 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ View to add product to the shopping cart with a quantity """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your bag')
+        messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -33,13 +34,16 @@ def add_to_cart(request, item_id):
 def adjust_cart(request, item_id):
     """ View to adjust quantity of product in the cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -49,12 +53,15 @@ def remove_from_cart(request, item_id):
     """ View to remove the product from the cart """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         cart = request.session.get('cart', {})
 
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
