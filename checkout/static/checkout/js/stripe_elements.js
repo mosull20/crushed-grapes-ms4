cdaec5155c4d547eeeca440b7357e00472500320
mosull20 <mosullivan@mac.com>
2022-01-21 11:28:10 +0000

    Add basic stripe payment functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,8 @@
 // Stripe - code taken from Boutique Ado videos
 
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-let client_secret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements({
     // solution to use google font in stripe card element found in stack overflow 
     // https://stackoverflow.com/questions/44915511/stripe-elements-google-web-font-not-working/47468682
@@ -29,3 +29,50 @@ let style = {
 };
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+  let errorDiv = document.getElementById('card-errors');
+  if (event.error) {
+    let html = `
+        <span class="icon mr-2" role="alert">
+          <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `;
+    $(errorDiv).html(html);
+  } else {
+      errorDiv.textContent = '';
+  }
+});
+
+// Handle submit payment to Stripe
+
+let form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  card.update({ 'disabled': true});
+  $('#payment-button').attr('disabled', true);
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+      card: card,
+    }
+  }).then(function(result) {
+    if (result.error) {
+        let errorDiv = document.getElementById('card-errors');
+        let html = `
+          <span class="icon mr-2" role="alert">
+            <i class="fas fa-times"></i>
+          </span>
+          <span>${result.error.message}</span>`;
+        $(errorDiv).html(html);
+        card.update({ 'disabled': false});
+        $('#payment-button').attr('disabled', false);
+    } else {
+      if (result.paymentIntent.status === 'succeeded') {
+        form.submit();
+      }
+    }
+  });
+});
