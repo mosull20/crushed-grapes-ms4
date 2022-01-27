b9abf046453b4741e6b702729bfb250075213d9e
mosull20 <mosullivan@mac.com>
2022-01-27 09:50:05 +0000

    Add confirmation email functionality

--- crushed_grapes/settings.py
+++ crushed_grapes/settings.py
@@ -174,6 +174,7 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'crushedgrapes@example.com'
 
 
 # Default primary key field type
