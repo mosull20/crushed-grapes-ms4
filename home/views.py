c700ed35a40cb5b102746206aa61a766ba006f3d
mosull20 <mosullivan@mac.com>
2022-02-12 13:32:19 +0000

    Add FAQs page to home app with content & add links in main nav and footer

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     """ View to render the home page """
     return render(request, 'home/index.html')
+
+
+def faqs(request):
+    """ View to render the faqs page """
+    return render(request, 'home/faqs.html')
